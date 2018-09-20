c204b33647631b7e3de803a763292ce7f6b75343
Nathan Greffe <N.Greffe@student.ulg.ac.be>
2018-09-20 15:41:41 +0200

    basic model

--- braet-greffe-in/src/RabbitsGrassSimulationModel.java
+++ braet-greffe-in/src/RabbitsGrassSimulationModel.java
@@ -1,4 +1,5 @@
 import uchicago.src.sim.engine.Schedule;
+import uchicago.src.sim.engine.SimInit;
 import uchicago.src.sim.engine.SimModelImpl;
 
 /**
@@ -12,35 +13,90 @@ import uchicago.src.sim.engine.SimModelImpl;
 
 
 public class RabbitsGrassSimulationModel extends SimModelImpl {		
+	private int gridSize = 20; // TODO: write in report we chose square because rectangles are not interesting 
+	private int initNumberRabbits = 20;
+	private int birthTreshold = 10;
+	private double grassGrowthRate = 0.005;
+	
+	private int grassEnergy = 5; // TODO: decide if they are variables or not, constants atm
+	private int movingCost = 1;
+	
+	private Schedule schedule;
+	private RabbitsGrassSimulationSpace rgsSpace;
+	
+	
+	public static void main(String[] args) {
+	    SimInit init = new SimInit();
+	    RabbitsGrassSimulationModel model = new RabbitsGrassSimulationModel();
+	    init.loadModel(model, "", false);
+	}
 
-		public static void main(String[] args) {
-			
-			System.out.println("Rabbit skeleton");
-			
-		}
+	public void begin() {
+		buildModel();
+	    buildSchedule();
+	    buildDisplay();
+	}
 
-		public void begin() {
-			// TODO Auto-generated method stub
-			
-		}
+	public void buildModel() {
+		System.out.println("Running BuildModel");
+		rgsSpace = new RabbitsGrassSimulationSpace(gridSize, initNumberRabbits);
+	}
+	
+	public void buildSchedule() {
+		System.out.println("Running BuildSchedule");
+	}
+	
+	public void buildDisplay() {
+		System.out.println("Running BuildDisplay");
+	}
 
-		public String[] getInitParam() {
-			// TODO Auto-generated method stub
-			return null;
-		}
+	public String[] getInitParam() {
+		String[] initParams = {"GridSize", "InitNumberRabbits", "BirthTreshold", "GrassGrowthRate"};
+		return initParams;
+	}
 
-		public String getName() {
-			// TODO Auto-generated method stub
-			return null;
-		}
+	public String getName() {
+		return "Rabbits and Grass";
+	}
 
-		public Schedule getSchedule() {
-			// TODO Auto-generated method stub
-			return null;
-		}
+	public Schedule getSchedule() {
+		return schedule;
+	}
 
-		public void setup() {
-			// TODO Auto-generated method stub
-			
-		}
+	public void setup() {
+		System.out.println("Running Setup");
+		rgsSpace = null;
+	}
+
+	public int getGridSize() {
+		return gridSize;
+	}
+
+	public void setGridSize(int gridSize) {
+		this.gridSize = gridSize;
+	}
+
+	public int getInitNumberRabbits() {
+		return initNumberRabbits;
+	}
+
+	public void setInitNumberRabbits(int initNumberRabbits) {
+		this.initNumberRabbits = initNumberRabbits;
+	}
+
+	public int getBirthTreshold() {
+		return birthTreshold;
+	}
+
+	public void setBirthTreshold(int birthTreshold) {
+		this.birthTreshold = birthTreshold;
+	}
+
+	public double getGrassGrowthRate() {
+		return grassGrowthRate;
+	}
+
+	public void setGrassGrowthRate(double grassGrowthRate) {
+		this.grassGrowthRate = grassGrowthRate;
+	}
 }
--- braet-greffe-in/src/RabbitsGrassSimulationSpace.java
+++ braet-greffe-in/src/RabbitsGrassSimulationSpace.java
@@ -1,8 +1,26 @@
+import uchicago.src.sim.space.Object2DGrid;
+
 /**
  * Class that implements the simulation space of the rabbits grass simulation.
  * @author 
  */
 
 public class RabbitsGrassSimulationSpace {
+	private Object2DGrid space;
+	
+	public RabbitsGrassSimulationSpace(int gridSize, int initNumberRabbits) {
+		space = new Object2DGrid(gridSize, gridSize);
+		for(int i = 0; i < gridSize; i++) {
+			for(int j = 0; j < gridSize; j++) {
+				space.putObjectAt(i, j, new Integer(0));
+			}
+		}
+		placeRabbits(initNumberRabbits);
+	}
+
+	private void placeRabbits(int initNumberRabbits) {
+		
+		
+	}
 
 }
